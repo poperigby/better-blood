edecdb11a24c202fdf256146e6d0acb3f939b02f
PopeRigby <poperigby@mailbox.org>
2025-01-01 19:12:50 -0800

    Add default blood_types def

--- scripts/better_blood/actor.lua
+++ scripts/better_blood/actor.lua
@@ -1,8 +1,19 @@
 local self = require("openmw.self")
 local types = require("openmw.types")
 
+local base_health = types.Actor.stats.dynamic.health(self).base
+local old_health = types.Actor.stats.dynamic.health(self).current
+
 local function onUpdate(_delta)
-    types.DynamicStats.health(self.object)
+    local new_health = types.Actor.stats.dynamic.health(self).current
+
+    local damage = old_health - new_health
+
+    old_health = new_health
+
+    if damage > 0 then
+        print(damage)
+    end
 end
 
 return {
