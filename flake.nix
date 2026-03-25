ae113f429ef9b069f23d33168d5cb8391757ea18
PopeRigby <poperigby@mailbox.org>
2026-03-25 10:08:52 -0700

    UHhhh

--- flake.nix
+++ flake.nix
@@ -9,11 +9,32 @@
         in
         {
             devShells = forEachSystem (
-                system: pkgs: {
+                system: pkgs:
+                let
+                    tealDeclarations =
+                        pkgs.runCommand "teal_declarations"
+                            {
+                                src = pkgs.fetchurl {
+                                    url = "https://gitlab.com/OpenMW/openmw/-/jobs/13631989749/artifacts/download";
+                                    sha256 = "sha256-R9GGBz5uIRmIHj62MuAKY3+BHlpR1UpoLg24vSqH0Ck=";
+                                };
+                            }
+                            ''
+                                mkdir -p $out
+                                mkdir -p temp
+                                ${lib.getExe pkgs.unzip} $src -d temp
+
+                                rm temp/teal_declarations/tlconfig.lua
+                                mv temp/teal_declarations/* $out/
+                            '';
+                in
+                {
                     default = pkgs.mkShell {
                         packages = with pkgs; [
                             luajitPackages.cyan
                         ];
+
+                        TEAL_DECLARATIONS = tealDeclarations;
                     };
                 }
             );
--- src/tlconfig.lua
+++ src/tlconfig.lua
@@ -1,5 +1,6 @@
 return {
     build_dir = "scripts",
     source_dir = "scripts",
-    include_dir = { ".teal_declarations" }
+    -- Read Teal declarations from the Nix store
+    include_dir = { os.getenv("TEAL_DECLARATIONS") }
 }
